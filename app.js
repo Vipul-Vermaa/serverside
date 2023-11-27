afa3c2f9f3a78e2c8d26911c9578ae24dc629414
Vipul <vipulvermalm8@gmail.com>
2023-11-28 00:14:45 +0530

    app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
 app.use(cookieParser())
 app.use(
     cors({
-        origin:process.env.FRONTEND_URL,
+        origin:"*",
         credentials:true,
         methods:['GET','POST','PUT','DELETE'],
     })
